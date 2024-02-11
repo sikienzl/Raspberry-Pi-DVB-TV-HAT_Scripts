06e75dbda8a99ce9fbcf994a0a8f9c4e3e2c16ef
Siegfried Kienzle <siegfried.kienzle@gmx.de>
2024-02-11 21:45:07 +0100

    add script
    add a script to manual download
    the DVB-T2 senderlist.

---
+++ scripts-for-manual-install/get_DE_senderlist_all.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+FILENAME="de-DVBT2"
+LINK="https://picockpit.com/raspberry-pi/wp-content/uploads/2022/10/aa-All.txt"
+
+# Test if the file exists
+if [ -f $FILENAME ]; then
+    echo "================================="
+    echo "| File $FILENAME already exists |"
+    echo "================================="
+    echo -en "\n"
+    read -p "Do you want to remove it? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        rm $FILENAME
+    else
+        echo "====================================="
+        echo "| The script will now be terminated |"
+        echo "====================================="
+        exit 1
+    fi
+fi
+
+# Test if the link is reachable
+if ! wget -q --spider --no-check-certificate $LINK; then
+    echo "========================="
+    echo "| Link is not reachable |"
+    echo "========================="
+    exit 1
+fi
+
+echo -en "\n"
+echo "==============================================="
+echo "| Start to download DE sender list for DVB-T2 |"
+echo "| from picockpit.com and save it as $FILENAME  |"
+echo "==============================================="
+
+# This script is used to get the DE sender list for DVB-T2
+# from picockpit.com and save it to a file.
+wget -O $FILENAME $LINK -q --show-progress --no-check-certificate
+
